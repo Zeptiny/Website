258b5e06da11e753d99fce7323d2f833baa4a9b3
Grentenville <arturtrentinibenedetti@gmail.com>
2024-05-30 20:27:34 -0300

    Comentário de cores não usadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,16 +8,16 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: { //TO-DO
+      colors: {
         'surface':'#121212',//Background
         'onSurface':'#D1D1D1',//Text on Background
-        'onSurfaceSM':'#A9B4C4',//Small Text on Background
+        //'onSurfaceSM':'#A9B4C4',//Small Text on Background
         'primary':'#242424',//Primary Color
         'onPrimary':'#F2F2F2',//Text on Primary Color
-        'secondary':'#6F6F6F',//Yada Yada...
-        'onSecondary':'#1A1A1A',
+        //'secondary':'#6F6F6F',//Yada Yada...
+        //'onSecondary':'#1A1A1A',
         'tertiary':'#E34234',
-        'onTertiary':'#F2F2F2',
+        //'onTertiary':'#F2F2F2',
       }
     },
   },
